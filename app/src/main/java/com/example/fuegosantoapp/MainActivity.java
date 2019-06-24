ad77168f1728a8b93e7554a33e2aefb8063bff18
Erick <erickjhorman@gmail.com>
2019-06-24 08:37:41 -0500

    Se modifica el logo

--- app/src/main/java/com/example/fuegosantoapp/MainActivity.java
+++ app/src/main/java/com/example/fuegosantoapp/MainActivity.java
@@ -9,5 +9,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Poner el icono en action bar
+
+        /*getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setIcon(R.mipmap.ic_launcher);*/
+
+
     }
 }
