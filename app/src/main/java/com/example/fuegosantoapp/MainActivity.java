edab3652a137308bbb459d557356f9a9bf627a0a
Erick <erickjhorman@gmail.com>
2019-07-01 10:43:09 -0500

    Se crea barra lateral de navegacion

--- app/src/main/java/com/example/fuegosantoapp/MainActivity.java
+++ app/src/main/java/com/example/fuegosantoapp/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.fuegosantoapp;
 
 import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Build;
 import android.os.Bundle;
@@ -12,7 +15,8 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-   Toolbar toolbar;
+    private DrawerLayout drawer; //Variable to make the funcionality of the navbar
+    Toolbar toolbar;
 
     @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
@@ -21,12 +25,31 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
-         //Crear logica del toolbar y asignarle un titulo
-         toolbar=(Toolbar) findViewById(R.id.toolbar_inicio);
-         setSupportActionBar(toolbar);
-         //toolbar.setTitle("Fuego Santo");
-         toolbar.setSubtitle("Dios les bendiga");
-         toolbar.setNavigationIcon(R.drawable.ic_menu_black_24dp);
+        //Crear logica del toolbar y asignarle un titulo
+        toolbar = (Toolbar) findViewById(R.id.toolbar_inicio);
+        setSupportActionBar(toolbar);
+        //toolbar.setTitle("Fuego Santo");
+        toolbar.setSubtitle("Dios les bendiga");
+        toolbar.setNavigationIcon(R.drawable.ic_menu_black_24dp);
+
+        //Crar funcionalidad del toolbar que abre y cierra la barra de navegacion
+        drawer = findViewById(R.id.drawer_layout);
+        drawer.setVerticalScrollBarEnabled(false);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar,
+                R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+
+
     }
 
     @Override
@@ -39,7 +62,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         String msg = "";
-        switch (item.getItemId()){
+        switch (item.getItemId()) {
 
             case R.id.search:
                 msg = "search";
@@ -58,7 +81,7 @@ public class MainActivity extends AppCompatActivity {
                 break;
 
         }
-        Toast.makeText(this, msg +  "Checked", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, msg + "Checked", Toast.LENGTH_SHORT).show();
         return super.onOptionsItemSelected(item);
     }
 }
