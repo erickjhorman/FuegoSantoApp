73fbd9d3a03826f95e397f92b270cbefb9dd6388
Erick <erickjhorman@gmail.com>
2019-07-02 07:35:48 -0500

    Se crea slider image show

--- app/src/main/java/com/example/fuegosantoapp/MainActivity.java
+++ app/src/main/java/com/example/fuegosantoapp/MainActivity.java
@@ -1,11 +1,13 @@
 package com.example.fuegosantoapp;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Build;
 import android.os.Bundle;
@@ -13,17 +15,28 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.fuegosantoapp.Slide_images.CustomSwipeAdapter;
+import com.example.fuegosantoapp.fragmentos.Fragmento_Mensaje;
+import com.example.fuegosantoapp.fragmentos.Fragmento_articulo;
+import com.example.fuegosantoapp.fragmentos.Fragmento_biblia;
+import com.example.fuegosantoapp.fragmentos.Fragmento_favoritos;
+import com.google.android.material.navigation.NavigationView;
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     private DrawerLayout drawer; //Variable to make the funcionality of the navbar
     Toolbar toolbar;
+    ViewPager viewPager;
+    CustomSwipeAdapter adapter;
 
     @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        viewPager = (ViewPager) findViewById(R.id.view_pager);
+        adapter = new CustomSwipeAdapter(this);
+        viewPager.setAdapter(adapter);
 
         //Crear logica del toolbar y asignarle un titulo
         toolbar = (Toolbar) findViewById(R.id.toolbar_inicio);
@@ -34,11 +47,25 @@ public class MainActivity extends AppCompatActivity {
 
         //Crar funcionalidad del toolbar que abre y cierra la barra de navegacion
         drawer = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
         drawer.setVerticalScrollBarEnabled(false);
+
+
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar,
                 R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
         toggle.syncState();
+
+        //Code for showing the articulos fragment at the start of the app
+
+        /*if (savedInstanceState == null) {
+
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                    new Fragmento_articulo()).commit();
+
+        }*/
     }
 
     @Override
@@ -84,4 +111,42 @@ public class MainActivity extends AppCompatActivity {
         Toast.makeText(this, msg + "Checked", Toast.LENGTH_SHORT).show();
         return super.onOptionsItemSelected(item);
     }
+
+    //To select the item of the navbar and open a specific fragmen
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+        switch (item.getItemId()) {
+            case R.id.nav_mensajes:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new Fragmento_Mensaje()).commit();
+                break;
+            case R.id.nav_publicaciones:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new Fragmento_articulo()).commit();
+                break;
+
+            case R.id.favoritos:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new Fragmento_favoritos()).commit();
+                break;
+
+            case R.id.biblia:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new Fragmento_biblia()).commit();
+                break;
+
+            case R.id.nav_share:
+                Toast.makeText(this, "Compartido", Toast.LENGTH_SHORT).show();
+                break;
+
+            case R.id.nav_send:
+                Toast.makeText(this, "Enviado", Toast.LENGTH_SHORT).show();
+                break;
+        }
+        drawer.closeDrawer(GravityCompat.START);
+
+        return true;
+    }
+
 }
---
+++ app/src/main/java/com/example/fuegosantoapp/Slide_images/CustomSwipeAdapter.java
@@ -0,0 +1,55 @@
+package com.example.fuegosantoapp.Slide_images;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.widget.LinearLayoutCompat;
+import androidx.viewpager.widget.PagerAdapter;
+
+import com.example.fuegosantoapp.R;
+
+public class CustomSwipeAdapter extends PagerAdapter {
+
+    private  int [] image_resources = {R.drawable.sample_1,R.drawable.sample_2,R.drawable.sample_3,R.drawable.sample_4, R.drawable.sample_5,R.drawable.sample_6,R.drawable.sample_8};
+    private Context ctx;
+    private LayoutInflater  layoutInflater;
+
+    public CustomSwipeAdapter(Context ctx ){
+          this.ctx = ctx;
+    }
+
+    @Override
+    public boolean isViewFromObject(@NonNull View view, @NonNull Object o) {
+        return ( view == (LinearLayout)o);
+
+    }
+
+    @Override
+    public int getCount() {
+        return image_resources.length;
+    }
+
+    @NonNull
+    @Override
+    public Object instantiateItem(@NonNull ViewGroup container, int position) {
+        layoutInflater = (LayoutInflater) ctx.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View item_view = layoutInflater.inflate(R.layout.slide_images_layout, container, false);
+        ImageView imageView = (ImageView) item_view.findViewById(R.id.image_view);
+        TextView textView = (TextView) item_view.findViewById(R.id.image_count);
+        imageView.setImageResource(image_resources[position]);
+        textView.setText("Image : "+ position);
+        container.addView(item_view);
+        return item_view;
+    }
+
+    @Override
+    public void destroyItem(@NonNull ViewGroup container, int position, @NonNull Object object) {
+        container.removeView((LinearLayout)object);
+    }
+}
